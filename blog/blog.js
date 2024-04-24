8bf72f623539973bdb694014c685a623a5e962c8
Jaydee <jerushdan.k@gmail.com>
2024-04-24 17:06:05 +0530

    macro optimizers

---
+++ blog/blog.js
@@ -0,0 +1,12 @@
+// Create a new image element
+var img = document.createElement('img');
+
+// Set the source of the image to the provided URL
+img.src = 'https://cdn.pixabay.com/photo/2016/12/26/17/28/spaghetti-1932466_1280.jpg';
+
+// Add some styling (optional)
+img.style.width = '100%';
+img.style.height = 'auto';
+
+// Append the image to the body or any other desired element
+document.body.appendChild(img);
